45bf918e6d2c0a80373bdcf82a4137b7a6075411
IrishEgie <irishegie.arao@ssu.edu.ph>
2024-11-06 21:55:19 +0800

    update: added todo list to application

--- test.py
+++ test.py
@@ -1,19 +1,32 @@
-import tkinter as tk
-from tkinter import ttk
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 
-def update_font_size(value):
-    try:
-        new_font_size = int(float(value))
-        label.config(font=("Roboto", new_font_size))
-    except ValueError:
-        pass  # Ignore non-integer values
-parent = tk.Tk()
-parent.title("Font Size Control")
-# Create a Scale widget for font size control
-font_size_scale = ttk.Scale(parent, from_=10, to=36, orient="horizontal", length=200, command=update_font_size)
-font_size_scale.pack(pady=10)
-# Create a label with initial font size
-initial_font_size = 16
-label = ttk.Label(parent, text="Font Size", font=("Roboto", initial_font_size))
-label.pack(pady=10)
-parent.mainloop()
+# Create figure
+fig = plt.figure(figsize=(10, 8))
+
+# Create GridSpec with custom layout (3 rows, 3 columns)
+gs = gridspec.GridSpec(nrows=3, ncols=3, figure=fig)
+
+# Define subplots within the GridSpec
+ax1 = fig.add_subplot(gs[0, 0])  # First row, first column
+ax2 = fig.add_subplot(gs[0, 1:])  # First row, spanning second and third columns
+ax3 = fig.add_subplot(gs[1:, 0])  # Second and third rows, first column
+ax4 = fig.add_subplot(gs[1:, 1:])  # Bottom-right section
+
+# Plot data
+ax1.plot([1, 2, 3], [1, 4, 9])
+ax2.plot([1, 2, 3], [9, 4, 1])
+ax3.plot([1, 2, 3], [3, 6, 9])
+ax4.plot([1, 2, 3], [1, 2, 3])
+
+# Set titles for each subplot
+ax1.set_title("Subplot 1")
+ax2.set_title("Subplot 2")
+ax3.set_title("Subplot 3")
+ax4.set_title("Subplot 4")
+
+# Adjust layout to prevent overlap
+plt.tight_layout()
+
+# Show plot
+plt.show()
